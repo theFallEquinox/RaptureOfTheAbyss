8fff1bb34f9b22ff54e040eb4bf11c101af2b2d1
theFallEquinox <equinoxatcurse@gmail.com>
2023-03-28 12:02:38 -0400

    Began the process of adding blocks and items. So far, I've added 3 blocks, and 4 items. (All of them will eventually be removed, as the textures aren't mine, they aren't fit for the mod, and their only true purpose is to practice coding with Java.)

---
+++ src/main/java/net/theFallEquinox/raptureOfTheAbyss/item/ModItems.java
@@ -0,0 +1,34 @@
+package net.theFallEquinox.raptureOfTheAbyss.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.theFallEquinox.raptureOfTheAbyss.RaptureOfTheAbyss;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, RaptureOfTheAbyss.MOD_ID);
+
+    // WEAPONS
+    public static final RegistryObject<Item> BASIC_KNIFE = ITEMS.register("basic_knife",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).durability(5)));
+
+    // RESOURCES
+    public static final RegistryObject<Item> MUDSTONE = ITEMS.register("mudstone_chunk",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    // TUTORIAL ITEMS
+    public static final RegistryObject<Item> COBALT_RAW = ITEMS.register("raw_cobalt",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
